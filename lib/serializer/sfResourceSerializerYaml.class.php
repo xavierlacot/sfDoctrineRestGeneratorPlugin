63e6e2397d81d04e3d17a6c291764006916f5199
xavierlacot <xavier@lacot.org>
2010-11-23 00:57:15 +0000

    [sfDoctrineRestGeneratorPlugin]: added a YAML serializer
    git-svn-id: http://svn.symfony-project.org/plugins/sfDoctrineRestGeneratorPlugin/trunk@31477 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/serializer/sfResourceSerializerYaml.class.php
@@ -0,0 +1,19 @@
+<?php
+
+class sfResourceSerializerYaml extends sfResourceSerializer
+{
+  public function getContentType()
+  {
+    return 'application/yaml';
+  }
+
+  public function serialize($array, $rootNodeName = 'data')
+  {
+    return sfYaml::dump(array($rootNodeName => $array), 5);
+  }
+
+  public function unserialize($payload)
+  {
+    return sfYaml::parse($array);
+  }
+}
