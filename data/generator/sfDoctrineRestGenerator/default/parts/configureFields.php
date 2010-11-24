b04e1b752a1a5b7feda8fbf767c85b4f0497488f
xavierlacot <xavier@lacot.org>
2010-11-24 23:52:39 +0000

    [sfDoctrineRestGeneratorPlugin]: Added configureFields() method, in order to share code between index and show actions (thanks dfeyer)
    git-svn-id: http://svn.symfony-project.org/plugins/sfDoctrineRestGeneratorPlugin/trunk@31500 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ data/generator/sfDoctrineRestGenerator/default/parts/configureFields.php
@@ -0,0 +1,44 @@
+  /**
+   * Allows to change configure some fields of the response, based on the
+   * generator.yml configuration file. Supported configuration directives are
+   * "date_format" and "tag_name"
+   *
+   * @return  void
+   */
+  protected function configureFields()
+  {
+<?php
+$fields = $this->configuration->getValue('default.fields');
+$specific_configuration_directives = false;
+
+foreach ($fields as $field => $configuration)
+{
+  if (isset($configuration['date_format']) || isset($configuration['tag_name']))
+  {
+    $specific_configuration_directives = true;
+    continue;
+  }
+}
+?>
+<?php if ($specific_configuration_directives): ?>
+    foreach ($this->objects as $i => $object)
+    {
+<?php foreach ($fields as $field => $configuration): ?>
+<?php if (isset($configuration['date_format']) || isset($configuration['tag_name'])): ?>
+      if (isset($object['<?php echo $field ?>']))
+      {
+<?php if (isset($configuration['date_format'])): ?>
+        $object['<?php echo $field ?>'] = date('<?php echo $configuration['date_format'] ?>', strtotime($object['<?php echo $field ?>']));
+<?php endif; ?>
+<?php if (isset($configuration['tag_name'])): ?>
+        $object['<?php echo $configuration['tag_name'] ?>'] = $object['<?php echo $field ?>'];
+        unset($object['<?php echo $field ?>']);
+<?php endif; ?>
+      }
+<?php endif; ?>
+<?php endforeach; ?>
+
+      $this->objects[$i] = $object;
+    }
+<?php endif; ?>
+  }
