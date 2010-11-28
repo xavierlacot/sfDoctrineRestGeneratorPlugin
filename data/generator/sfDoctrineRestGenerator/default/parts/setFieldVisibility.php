0c674e63a62ec29301a6719626b8c815df5b85a1
xavierlacot <xavier@lacot.org>
2010-11-28 17:10:26 +0000

    [sfDoctrineRestGeneratorPlugin]: fixed the way relation fields are hidden, particularly in the case of many-to-many relationships
    git-svn-id: http://svn.symfony-project.org/plugins/sfDoctrineRestGeneratorPlugin/trunk@31548 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/generator/sfDoctrineRestGenerator/default/parts/setFieldVisibility.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/setFieldVisibility.php
@@ -42,15 +42,40 @@ $object_additional_fields = $this->configuration->getValue('get.object_additiona
 
     $embedded_relations_hide = <?php echo var_export($embedded_relations_hide, true); ?>;
 
+    // relation types
+    $relation_types = array(
+<?php foreach (array_keys($embedded_relations_hide) as $relation_name): ?>
+<?php
+$relation = Doctrine::getTable($this->getModelClass())->getRelation($relation_name);
+?>
+  '<?php echo $relation_name ?>' => <?php echo var_export(Doctrine_Relation::MANY == $relation->getType()) ?>,
+<?php endforeach; ?>
+    );
+
     foreach ($this->objects as $i => $object)
     {
+      // only filter objects, not additional fields
       if (is_int($i))
       {
         foreach ($embedded_relations_hide as $relation_name => $hidden_fields)
         {
-          if (isset($object[$relation_name]))
+          if (false === $relation_types[$relation_name])
           {
-            $object[$relation_name] = array_diff_key($object[$relation_name], $hidden_fields);
+            if (isset($object[$relation_name]))
+            {
+              $object[$relation_name] = array_diff_key($object[$relation_name], $hidden_fields);
+            }
+          }
+          else
+          {
+            $related_objects = $object[$relation_name];
+
+            foreach ($related_objects as $j => $related_object)
+            {
+              $related_objects[$j] = array_diff_key($related_object, $hidden_fields);
+            }
+
+            $object[$relation_name] = $related_objects;
           }
         }
 
