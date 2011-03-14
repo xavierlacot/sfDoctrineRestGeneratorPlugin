1f7b5432fb26d18614170960d326b5f91e3f5422
damienalexandre <damien@damienalexandre.fr>
2011-03-14 10:36:25 +0000

    [sfDoctrineRestGeneratorPlugin] Make the XML deserialization less permisive but add support for XML errors, displaying more informations about a Payload parsing error.
    git-svn-id: http://svn.symfony-project.org/plugins/sfDoctrineRestGeneratorPlugin/trunk@32251 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/serializer/sfResourceSerializerXml.class.php
+++ lib/serializer/sfResourceSerializerXml.class.php
@@ -13,13 +13,46 @@ class sfResourceSerializerXml extends sfResourceSerializer
     return $this->arrayToXml($array, $camelizedRootNodeName, 0, $collection);
   }
 
+  /**
+   * Transform the payload into array assuming the payload is XML formatted.
+   * 
+   * @param string $payload
+   * @return array
+   * @throw Exception
+   */
   public function unserialize($payload)
   {
-    $return = $this->unserializeToArray(@simplexml_load_string(
+    libxml_use_internal_errors(true);
+    $payload = trim($payload);
+
+    if (empty($payload))
+    {
+      throw new sfException("Empty payload, can't unserialize it.");
+    }
+
+    // Try to parse the XML
+    $xml = @simplexml_load_string(
       $payload,
       'SimpleXMLElement',
-      LIBXML_NOERROR || LIBXML_NOWARNING || LIBXML_NONET
-    ));
+      LIBXML_NONET
+    );
+
+    // If false, there is a parse error.
+    if ($xml === false)
+    {
+      $errors = libxml_get_errors();
+      $exception_message = '';
+      
+      foreach ($errors as $error)
+      {
+        $exception_message .= $this->formatXmlError($error);
+      }
+
+      libxml_clear_errors();
+      throw new sfException("XML parsing error(s): \n".$exception_message);
+    }
+    
+    $return = $this->unserializeToArray($xml);
 
     // Shift any root node and return only the nested array
     if (is_array($return) && count($return) == 1)
@@ -37,10 +70,41 @@ class sfResourceSerializerXml extends sfResourceSerializer
     return $return;
   }
 
+  /**
+   * Return a formatted LibXml Error message
+   * @see http://www.php.net/manual/en/function.libxml-get-errors.php
+   * @param LibXMLError $error
+   * @return string
+   */
+  protected function formatXmlError($error)
+  {
+    $return  = "\n\n";
+    
+    switch ($error->level)
+    {
+      case LIBXML_ERR_WARNING:
+        $return .= "Warning $error->code: ";
+        break;
+      case LIBXML_ERR_ERROR:
+        $return .= "Error $error->code: ";
+        break;
+      case LIBXML_ERR_FATAL:
+        $return .= "Fatal Error $error->code: ";
+        break;
+    }
+
+    $return .= trim($error->message) . "\n  Line: $error->line" .  "\n  Column: $error->column";
+
+    if ($error->file)
+    {
+        $return .= "\n  File: $error->file";
+    }
+
+    return "$return\n\n--------------------------------------------\n\n";
+  }
+
   protected function unserializeToArray($data)
   {
-    libxml_use_internal_errors(true);
-
     if ($data instanceof SimpleXMLElement)
     {
       $data = (array) $data;
